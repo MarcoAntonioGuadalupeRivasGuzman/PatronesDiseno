e2c7b2aeda46914003f2415304321990129148a5
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-01-07 20:36:57 -0600

    clase que implementa el interface button

---
+++ Factory/HtmlButton.java
@@ -0,0 +1,11 @@
+public class HtmlButton implements Button {
+    public void render(){
+        System.out.println("<button>Test Button</button>");
+    }
+
+    public void onClick(){
+        System.out.println("Click! Button says - Hello World!");
+    }
+
+    // implementacion html de la interfaz button
+}
