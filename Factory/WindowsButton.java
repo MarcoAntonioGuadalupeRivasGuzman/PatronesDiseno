6cd9605a26f3898c32d049163932b10859754016
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-01-07 20:39:05 -0600

    clase que implementa el interface button

---
+++ Factory/WindowsButton.java
@@ -0,0 +1,12 @@
+public class WindowsButton implements Button{
+    
+    public void render(){
+        System.out.println("I am button for windows");
+    }
+
+    public void onClick(){
+        System.out.println("Click! Button says - Hello World!");
+    }
+
+    // implementacion en windos de la interfaz button
+}
